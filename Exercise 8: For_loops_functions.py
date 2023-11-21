87af8f02c3bf779f9b21e60e940df6d521b8f5e5
20prince20 <145338628+20prince20@users.noreply.github.com>
2023-11-21 15:08:59 +0800

    Update Exercise 8: For_loops_functions.py

--- Exercise 8: For_loops_functions.py
+++ Exercise 8: For_loops_functions.py
@@ -6,5 +6,10 @@ def print_odd_numbers():
   for i in range(1, 100):
       if i % 2 != 0:
           print(i)
-print_even_numbers()
-print_odd_numbers()
+
+value = input("odd or even?")
+if value == "odd":
+  print_odd_numbers()
+else:
+  print_even_numbers()
+  
