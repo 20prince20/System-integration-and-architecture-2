b5663fbfdbefd659d6091b94f4a57beff4647dbf
20prince20 <145338628+20prince20@users.noreply.github.com>
2023-11-21 15:21:50 +0800

    Update Exercise 8: For_loops_functions.py

--- Exercise 8: For_loops_functions.py
+++ Exercise 8: For_loops_functions.py
@@ -1,9 +1,12 @@
 #Exercise 7: Odd_or_even_functions.py
 
 def even_or_odd():
-    value = input("What Number? ")
-    if int(value) % 2 == 0:
-        print(f"{value} is even")
-    else:
-        print(f"{value} is odd")
-even_or_odd()
+    # Displaying even numbers from 1 to 100 using a for loop
+for i in range(1, 101):
+    if number % 2 == 0:
+        print(i)
+
+# Displaying odd numbers from 1 to 100 using a for loop
+for i in range(1, 101):
+        if number % 2 != 0:
+            print(i)
