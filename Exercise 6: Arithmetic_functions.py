538b0acedb9d4ff3ea552a546734faefaa106c68
20prince20 <145338628+20prince20@users.noreply.github.com>
2023-11-21 15:20:39 +0800

    Update Exercise 6: Arithmetic_functions.py

--- Exercise 6: Arithmetic_functions.py
+++ Exercise 6: Arithmetic_functions.py
@@ -1,14 +1,33 @@
 def display_num(num1, num2):
-    sum = num1 + num2
-    difference = num1 - num2
-    quotient = num1 / num2
-    product = num1 * num2
+    #addition
+num1 = 20
+num2 = 30
 
-    print("The sum of", num1 ,"and" ,num2, "is equal to", sum)
-    print("The difference of", num1, "and", num2, "is equal to", difference)
-    print("The quotient of", num1, "and", num2, "is equal to", quotient)
-    print("The product of", num1, "and", num2, "is equal to", product)
+sum = num1 + num2
 
-num1 = int(input("Enter num1: "))
-num2 = int(input("Enter num2: "))
-display_num(num1, num2)
+print ("the sum is ",sum)
+
+#division
+
+num3 = 40
+num4 = 50
+
+divide = num3 / num4
+
+print ("the divide is ",divide)
+
+#subtraction
+num5 = 100
+num6 = 70
+
+subtrac = num5 - num6
+
+print ("the subtrac is ",subtrac)
+
+#multiplication
+num7 = 70
+num8 = 80
+
+multiply = num7 * num8
+
+print ("the multiply is ",multiply)
